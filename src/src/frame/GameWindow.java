103e0241a396fee32808ecd954eaeecd2c7bcf90
elecmarsynth <lun.tonlill16@gmail.com>
2026-02-25 22:05:17 +0700

    Update GameWindow.java
    format code

--- src/src/frame/GameWindow.java
+++ src/src/frame/GameWindow.java
@@ -1,4 +1,5 @@
 package frame;
+
 import Tower.PlantTower;
 import UI.ImageLoader;
 import UI.TowerMenu;
@@ -8,34 +9,38 @@ import java.awt.CardLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import panelCore.GamePanel;
+
 public class GameWindow {
     private CardLayout cardLayout = new CardLayout();
     private JPanel bottomPanel = new JPanel(cardLayout);
     private TowerMenu towerMenu;
     private UpgradeMenu upgradeMenu;
-public GameWindow() {
-    JFrame window = new JFrame();
-    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    window.setLayout(new BorderLayout());
-    ImageLoader.loadAll();
-    GamePanel gamePanel = new GamePanel(this);
-    window.add(gamePanel, BorderLayout.CENTER);
-    towerMenu = new TowerMenu(gamePanel, cardLayout, bottomPanel);
-    upgradeMenu = new UpgradeMenu(gamePanel, cardLayout, bottomPanel,towerMenu);
-    bottomPanel.add(towerMenu, "TowerMenu");
-    bottomPanel.add(upgradeMenu, "UpgradeMenu");
-    window.add(bottomPanel, BorderLayout.SOUTH);
-    window.pack();                      
-    // window.setSize(1920, 720);
-    window.setLocationRelativeTo(null); 
-    window.setVisible(true);
 
-    System.out.println(window.getWidth() + "x" + window.getHeight());
+    public GameWindow() {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLayout(new BorderLayout());
+        ImageLoader.loadAll();
+        GamePanel gamePanel = new GamePanel(this);
+        window.add(gamePanel, BorderLayout.CENTER);
+        towerMenu = new TowerMenu(gamePanel, cardLayout, bottomPanel);
+        upgradeMenu = new UpgradeMenu(gamePanel, cardLayout, bottomPanel, towerMenu);
+        bottomPanel.add(towerMenu, "TowerMenu");
+        bottomPanel.add(upgradeMenu, "UpgradeMenu");
+        window.add(bottomPanel, BorderLayout.SOUTH);
+        window.pack();
+        // window.setSize(1920, 720);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        System.out.println(window.getWidth() + "x" + window.getHeight());
     }
+
     public void showUpgrade(PlantTower tower) {
-    upgradeMenu.showUpgrade(tower); 
+        upgradeMenu.showUpgrade(tower);
     }
+
     public void showTower() {
-        towerMenu.showTower(); 
-    }    
+        towerMenu.showTower();
+    }
 }
